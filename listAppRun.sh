64f9f1846a63241ca6e71f0cd2227010d4033305
shavez00 <shavez00@yahoo.com>
2017-04-18 22:19:38 +0000

    modified the listAppRun script to accept only integers for port numbers

--- listAppRun.sh
+++ listAppRun.sh
@@ -1,13 +1,16 @@
+#the line below captures the first option entered "$1" and assigns it to the "port" varible
 port=$1
 
-if [[ -n "$port" ]]; then
+#this checks if the varible "port" is non-zero
+if [[ "$port" -gt 0 ]]; then
   
 docker network create -d bridge list-network
 docker run -d --name listDb --network="list-network" -e MYSQL_ROOT_PASSWORD=July,252014 -e MYSQL_DATABASE=grList mysql:8
+#assigns the varible "port" to the first option of the docker run flag "-p"
 docker run -d --name list --network="list-network" -p $port:80 -v "$PWD":/var/www/localhost/htdocs/list shavez00/alpine-apache2-php:withPdoForMysqlSupport
 
 else
 
-echo "You need to specify a port that you want the Grocery List app to run on"
+echo "You need to specify a port that you want the Grocery List app to run on, the port number needs to be a integer.  Example ./listAppRun.sh 8080"
 
 fi
