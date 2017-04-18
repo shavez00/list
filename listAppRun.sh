ae2406da83640b4349ed7982873a43a608a4533a
shavez00 <shavez00@yahoo.com>
2017-04-18 22:09:31 +0000

    updated listAppRun script to accept port number that you want the app to run on

--- listAppRun.sh
+++ listAppRun.sh
@@ -1,3 +1,13 @@
+port=$1
+
+if [[ -n "$port" ]]; then
+  
 docker network create -d bridge list-network
 docker run -d --name listDb --network="list-network" -e MYSQL_ROOT_PASSWORD=July,252014 -e MYSQL_DATABASE=grList mysql:8
-docker run -d --name list --network="list-network" -p 8082:80 -v "$PWD":/var/www/localhost/htdocs/list shavez00/alpine-apache2-php:withPdoForMysqlSupport
+docker run -d --name list --network="list-network" -p $port:80 -v "$PWD":/var/www/localhost/htdocs/list shavez00/alpine-apache2-php:withPdoForMysqlSupport
+
+else
+
+echo "You need to specify a port that you want the Grocery List app to run on"
+
+fi
