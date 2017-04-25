d78e32e151942465515a4e0184f73c097513e233
shavez00 <shavez00@Yahoo.com>
2017-04-25 15:35:34 -0700

    Adding that a user/pass need to passed to the script

--- listAppRun.sh
+++ listAppRun.sh
@@ -1,5 +1,7 @@
 #the line below captures the first option entered "$1" and assigns it to the "port" varible
 port=$1
+user=$2
+pass=$3
 
 #this checks if the varible "port" is non-zero
 if [[ "$port" -gt 0 ]]; then
@@ -8,13 +10,22 @@ if [[ "$port" -gt 0 ]]; then
 docker network create -d bridge list-network
 #create the mysql server
 docker run -d --name listDb --network="list-network" -v "$PWD":/var/list/mysql shavez00/alpine-mysql
-#create the database and tables needed
-docker exec -it listDb /var/lib/mysql/createDb.sh
+
 #assigns the varible "port" to the first option of the docker run flag "-p"
 docker run -d --name list --network="list-network" -p $port:80 -v "$PWD":/var/www/localhost/htdocs/list shavez00/alpine-apache2-php:withPdoForMysqlSupport
 
 else
 
 echo "You need to specify a port that you want the Grocery List app to run on, the port number needs to be a integer.  Example ./listAppRun.sh 8080"
+
+fi
+
+if [[ "$user" -gt 0 ]]; then
+#create the database and tables needed
+docker exec -it listDb /var/lib/mysql/createDb.sh $2 $3
+
+else
+
+echo "You need to enter in a username and password for the database user"
 
 fi
