88df74aa6d51281cc8709b0f1d1936676f46b98a
shavez00 <shavez00@Yahoo.com>
2017-04-25 15:40:16 -0700

    created db user

--- createDb.sh
+++ createDb.sh
@@ -16,3 +16,15 @@ measure varchar(255),
 PRIMARY KEY (itemId));"
 
 mysql -e "$TABLE"
+
+user=$1
+pass=$2
+
+if [[ "$user" -gt 0 ]]; then
+
+USER="CREATE USER '$user'@'list.list-network' IDENTIFIED BY '$pass';
+GRANT ALL PRIVLEGES on grList.* TO '$user'@list.list-network';
+FLUSH PRIVLEGES;
+REVOKE DROP ON grList.* FROM '$user'@'list.list-network';"
+
+mysql -e "$USER"
