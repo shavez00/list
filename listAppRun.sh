90eafa33b1146cf00324bd0ee09ef5bb616f06af
shavez00 <shavez00@Yahoo.com>
2017-04-25 15:16:55 -0700

    Added Db creation script

--- listAppRun.sh
+++ listAppRun.sh
@@ -3,9 +3,13 @@ port=$1
 
 #this checks if the varible "port" is non-zero
 if [[ "$port" -gt 0 ]]; then
-  
+
+#create the docker bridge network that the database and webserver will run on
 docker network create -d bridge list-network
+#create the mysql server
 docker run -d --name listDb --network="list-network" -v "$PWD":/var/list/mysql shavez00/alpine-mysql
+#create the database and tables needed
+docker exec -it listDb /var/lib/mysql/createDb.sh
 #assigns the varible "port" to the first option of the docker run flag "-p"
 docker run -d --name list --network="list-network" -p $port:80 -v "$PWD":/var/www/localhost/htdocs/list shavez00/alpine-apache2-php:withPdoForMysqlSupport
 
