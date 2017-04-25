93d781907918925bf75aad8a5f1c9bc339270e9a
shavez00 <shavez00@Yahoo.com>
2017-04-25 16:36:06 -0700

    Update createDb.sh

--- createDb.sh
+++ createDb.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+user=$1
+pass=$2
+
+if [[ "$user" != "" || "$pass" != "" ]]; then
+
+USER="CREATE USER '$user'@'list.list-network' IDENTIFIED BY '$pass';
+GRANT ALL PRIVLEGES on grList.* TO '$user'@list.list-network';
+FLUSH PRIVLEGES;
+REVOKE DROP ON grList.* FROM '$user'@'list.list-network';"
+
+mysql -e "$USER"
+
+else
+
+echo "You need to set a username and password for the datbase user.  ex ./createDb.sh username password"
+exit
+
+fi
+
 CREATE="create database grList;"
 
 mysql -e "$CREATE"
@@ -16,21 +35,3 @@ measure varchar(255),
 PRIMARY KEY (itemId));"
 
 mysql -e "$TABLE"
-
-user=$1
-pass=$2
-
-if [[ "$user" != "" || "$pass" != "" ]]; then
-
-USER="CREATE USER '$user'@'list.list-network' IDENTIFIED BY '$pass';
-GRANT ALL PRIVLEGES on grList.* TO '$user'@list.list-network';
-FLUSH PRIVLEGES;
-REVOKE DROP ON grList.* FROM '$user'@'list.list-network';"
-
-mysql -e "$USER"
-
-else
-
-echo "You need to set a username and password for the datbase user.  ex ./createDb.sh username password"
-
-fi
