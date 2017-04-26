f1ed5c968196ffd964019592e4a6ee0339e069d4
shavez00 <shavez00@Yahoo.com>
2017-04-25 17:01:55 -0700

    Update createDb.sh

--- createDb.sh
+++ createDb.sh
@@ -3,22 +3,6 @@
 user=$1
 pass=$2
 
-if [[ "$user" != "" || "$pass" != "" ]]; then
-
-USER="CREATE USER '$user'@'list.list-network' IDENTIFIED BY '$pass';
-GRANT ALL PRIVILEGES on grList.* TO '$user'@'list.list-network';
-FLUSH PRIVILEGES;
-REVOKE DROP ON grList.* FROM '$user'@'list.list-network';"
-
-mysql -e "$USER"
-
-else
-
-echo "You need to set a username and password for the datbase user.  ex ./createDb.sh username password"
-exit
-
-fi
-
 CREATE="create database grList;"
 
 mysql -e "$CREATE"
@@ -35,3 +19,21 @@ measure varchar(255),
 PRIMARY KEY (itemId));"
 
 mysql -e "$TABLE"
+
+if [[ "$user" != "" || "$pass" != "" ]]; then
+
+USER="CREATE USER '$user'@'list.list-network' IDENTIFIED BY '$pass';
+GRANT ALL PRIVILEGES on grList.* TO '$user'@'list.list-network';
+FLUSH PRIVILEGES;
+REVOKE DROP ON grList.* FROM '$user'@'list.list-network';"
+
+mysql -e "$USER"
+
+else
+
+echo "You need to set a username and password for the datbase user.  ex ./createDb.sh username password"
+DROP="DROP DATABASE grList;"
+
+mysql -e "$DROP"
+
+fi
